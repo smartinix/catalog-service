50d7b9dd3b1f5a2dddbdf498e08872d67050ee7d
Nenad Martinovic <nenad.martinovic@enjoying.rs>
2023-08-31 16:48:45 +0200

    Added testdata profile and configuration to support it, lo load initial data in local development mode

---
+++ src/main/java/com/smartinix/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,31 @@
+package com.smartinix.catalogservice.demo;
+
+import com.smartinix.catalogservice.domain.Book;
+import com.smartinix.catalogservice.domain.BookRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.BitSet;
+
+@Component
+@Profile("testdata")
+@ConditionalOnProperty(name = "polar.testdata.enabled", havingValue = "true")
+public class BookDataLoader {
+    private final BookRepository bookRepository;
+
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 = new Book("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
+        var book2 = new Book("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+
+}
