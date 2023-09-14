e73dbd85bd567d6c7b1158895aff9ec9ac319464
Nenad Martinovic <nenad.martinovic@enjoying.rs>
2023-09-14 12:43:40 +0200

    Updated error message in exception.

--- src/main/java/com/smartinix/catalogservice/domain/BookAlreadyExistsException.java
+++ src/main/java/com/smartinix/catalogservice/domain/BookAlreadyExistsException.java
@@ -2,6 +2,6 @@ package com.smartinix.catalogservice.domain;
 
 public class BookAlreadyExistsException extends RuntimeException {
     public BookAlreadyExistsException(String isbn) {
-        super("A book with ISBN " + isbn + "already exists.");
+        super("A book with ISBN " + isbn + " already exists.");
     }
 }
