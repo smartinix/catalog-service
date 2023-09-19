5887c323ee24bd2616a04b006aeb847a72d7e0c2
Nenad Martinovic <nenad.martinovic@enjoying.rs>
2023-09-19 18:52:39 +0200

    Replaced the depricated method with valid one from Spring Security v6

--- src/main/java/com/smartinix/catalogservice/config/SecurityConfig.java
+++ src/main/java/com/smartinix/catalogservice/config/SecurityConfig.java
@@ -2,10 +2,10 @@ package com.smartinix.catalogservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
@@ -20,7 +20,8 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/", "/books/**").permitAll()
                 .anyRequest().hasRole("employee")
             )
-            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+            .oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(Customizer.withDefaults()))
             .sessionManagement(sessionManagement ->
                                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .csrf(AbstractHttpConfigurer::disable)
